c6adc0db0d34c7785eaafb0600ed67abe056b7b4
Jose Aguilera <jaguilera@electroram.onmicrosoft.com>
2026-07-21 00:54:38 -0400

    agregar vite config

---
+++ src/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 5000,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
+})
