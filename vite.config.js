ffd574a644b1629d25f3b5debb55c0ff8ccbe936
Jose Aguilera <jaguilera@electroram.onmicrosoft.com>
2026-07-21 02:24:42 -0400

    Revert "fix safari movil"
    This reverts commit bd3bc6a1092c1a13d33da61277e640405dac5c89.

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     chunkSizeWarningLimit: 5000,
-    cssMinify: false,
-    minify: false,
-    target: ['es2015', 'safari11'],
     rollupOptions: {
       output: {
         manualChunks: undefined
